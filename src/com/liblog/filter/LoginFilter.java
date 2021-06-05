a4fc638988451e230ce3ee0568acc840bfc5bea5
prpshinol <hengping19782@126.com>
2021-06-05 22:17:12 +0800

    修改自动登录（记住一周）的bug

--- src/com/liblog/filter/LoginFilter.java
+++ src/com/liblog/filter/LoginFilter.java
@@ -22,28 +22,49 @@ public class LoginFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        if (request.getRequestURI().contains("/login")) {//登录Controller，不需要过滤
+        if (request.getRequestURI().contains("/login/admin")) {//后台登录注册
             filterChain.doFilter(request, response);
-        } else {
-            if (request.getRequestURI().contains("/admin")) {//后台
-                if (request.getSession().getAttribute("loginAdmin") != null) {//管理员已登录
-                    filterChain.doFilter(request, response);
-                } else {//管理员未登录
-                    response.sendRedirect(request.getContextPath() + "/login/adminLoginUI.do");
-                }
-            } else {//前台
-                //执行自动登陆的操作
-                LoginCookieUtil.readCookieAndLogin(request, response);
-
-                if (request.getSession().getAttribute("loginUser") != null) {//已登录
-                    filterChain.doFilter(request, response);
-                } else {//未登录
-                    response.sendRedirect(request.getContextPath() + "/login/loginUI.do");
-                }
+        } else if (request.getRequestURI().contains("/admin")) {//后台页面
+            if (request.getSession().getAttribute("loginAdmin") != null) {//管理员已登录
+                filterChain.doFilter(request, response);
+            } else {//管理员未登录
+                response.sendRedirect(request.getContextPath() + "/login/adminLoginUI.do");
             }
-
+        } else {//前台
+            //执行自动登陆的操作
+            boolean autoLogin = LoginCookieUtil.readCookieAndLogin(request, response);
+            if(autoLogin){
+                if (isPublicForUser(request)) {
+                    //自动登陆的情况下，这些uri不可访问，直接跳转到首页
+                    response.sendRedirect(request.getContextPath()+"/home/indexUI.do");
+                }else {
+                    //其他情况下，已经登陆，放行
+                    filterChain.doFilter(request, response);
+                }
+            } else if(isPublicForUser(request)){//前台登录注册
+                filterChain.doFilter(request, response);
+            } else if (request.getSession().getAttribute("loginUser") != null) {//已登录
+                filterChain.doFilter(request, response);
+            } else {//未登录
+                response.sendRedirect(request.getContextPath() + "/login/loginUI.do");
+            }
         }
+    }
 
+    /**
+     * 判断uri是否是用户登陆注册时用到的(自动登陆时这些uri不可通过)
+     * @param request
+     * @return
+     */
+    private boolean isPublicForUser(HttpServletRequest request){
+        if (request.getRequestURI().contains("login/loginUI")
+                ||request.getRequestURI().contains("login/registerUI")
+                ||request.getRequestURI().contains("login/login")
+                ||request.getRequestURI().contains("login/register")
+                ||request.getRequestURI().contains("login/getCode")) {
+            return true;
+        }
+        return false;
     }
 
     @Override
